e74f9b720bb2f009a687de615d50d3704690f8ea
henmanuel <admin@loopzone.org>
2018-07-10 08:54:03 -0600

    Jira Support APP

--- app/assets/js/bundle.js
+++ app/assets/js/bundle.js
@@ -498,8 +498,7 @@ class app {
                             if(companies[company][branch].hasOwnProperty(type)){
                                 for(let issue in companies[company][branch][type]){
                                     if(companies[company][branch][type].hasOwnProperty(issue)){
-                                        data[company];
-                                        data[company].push(issue);
+                                        data[company][issue] = companies[company][branch][type][issue];
                                     }
                                 }
                             }
